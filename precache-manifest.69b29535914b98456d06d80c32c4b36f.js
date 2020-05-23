f3ce82c6dcd5d0fab170b8a13ee1ca69edca9963
gianniop <63250523+gianniop@users.noreply.github.com>
2020-05-23 22:22:04 +0300

    Add files via upload

---
+++ precache-manifest.69b29535914b98456d06d80c32c4b36f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150c07414e436c073afba46b30dd9dcc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed649b9a01a3d155b7b5",
+    "url": "./static/css/main.27157408.chunk.css"
+  },
+  {
+    "revision": "f37a71018ba9ac3452ab",
+    "url": "./static/js/2.feadacf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.feadacf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed649b9a01a3d155b7b5",
+    "url": "./static/js/main.875fc8db.chunk.js"
+  },
+  {
+    "revision": "c3af92f31ca245857115",
+    "url": "./static/js/runtime-main.d9cb5643.js"
+  }
+]);
